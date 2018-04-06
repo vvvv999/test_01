e0ccfc52c18edcc43f122198424f1256f79f3592
vvvv999 <38009040+vvvv999@users.noreply.github.com>
2018-04-06 18:30:11 +0300

    added sort cars

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,20 @@
+import cars.Car;
 import taxistation.TaxiStation;
+
+import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
         TaxiStation station1 = new TaxiStation();
         station1.addDefaultCars();
-        System.out.println(station1.carTotalValue());
+      //  System.out.println(station1.carTotalValue());
 
+
+        List<Car> sortedCarList = station1.sortCarsList();
+
+
+        for (Car car : sortedCarList){
+            System.out.println(car);
+        }
     }
-
-
 }
--- src/taxistation/TaxiStation.java
+++ src/taxistation/TaxiStation.java
@@ -2,8 +2,10 @@ package taxistation;
 
 import cars.Car;
 import cars.HatchbackCar;
-
+import cars.SedanCar;
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
 
 public class TaxiStation {
@@ -12,19 +14,52 @@ public class TaxiStation {
 
 
     //for testing purposes
-    public void addDefaultCars(){
-        Car car1 = new HatchbackCar("Mercedes", "w123", (short)1990,
+    public void addDefaultCars() {
+        Car car1 = new HatchbackCar("Mercedes", "w123", (short) 1990,
                 "prr123", 34.3, false, 12000);
         carsList.add(car1);
+        Car car2 = new SedanCar("Ford", "focus", (short) 2000,
+                "prr23123", 25, false, 15000);
+        carsList.add(car2);
+
+        for (Car car : carsList) {
+            System.out.println(car);;
+        }
+        System.out.println();
     }
 
 
+    // find total value of Taxi Station cars
     public double carTotalValue() {
         double totalValue = 0.0;
-        for (Car car: carsList){
-           totalValue += car.getValue();
+        for (Car car : carsList) {
+            totalValue += car.getValue();
         }
         return totalValue;
     }
 
+//sort cars list
+    public List<Car> sortCarsList() {
+
+        List<Car> sortedCarsList = carsList;
+        Collections.sort(sortedCarsList, new Comparator<Car>() {
+            @Override
+            public int compare(Car o1, Car o2) {
+                return ((int) Math.round(100.0 * (o1.getFuelConsumption() - o2.getFuelConsumption())));
+            }
+        });
+        return sortedCarsList;
+
+    }
+//find a car by year and value specific range
+    public Car findCarByAttributes() {
+
+
+        return null;
+    }
 }
+
+
+
+
+
