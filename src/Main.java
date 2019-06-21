127e674ed3b9658211a67d5f025f95b99b93f9d2
EPBYMINW5128$@minsk.epam.com <EPBYMINW5128$@minsk.epam.com>
2019-06-21 14:49:52 +0300

    'test'

--- src/Main.java
+++ src/Main.java
@@ -6,7 +6,7 @@ import java.io.IOException;
 
 public class Main {
     public static void main(String[] args) {
-
+//test
         TaxiStation station1 = new TaxiStation();
         station1.sortCarsList();
 //      DataReader dbReader = new DBReader();
